b1c60cdc0342e2558b62b1891448aae298a768af
Samira Tavakolli <104760134+samiracode@users.noreply.github.com>
2023-10-12 22:02:38 +0200

    isPositive function

--- day3.js
+++ day3.js
@@ -61,5 +61,22 @@ function reverseString(s) {
 }
   
 // Test the function with different inputs
-  reverseString("Hello, World!"); // Reverses and prints the string
-  reverseString(1234); // Outputs "s.split is not a function"
+reverseString("Hello, World!"); // Reverses and prints the string
+reverseString(1234); // Outputs "s.split is not a function"
+
+/*
+ * Complete the isPositive function.
+ * If 'a' is positive, return "YES".
+ * If 'a' is 0, throw an Error with the message "Zero Error"
+ * If 'a' is negative, throw an Error with the message "Negative Error"
+*/
+function isPositive(a) {
+    try {
+        if ( a < 0 ) throw new Error("Negative Error")
+        if ( a === 0) throw new Error("Zero Error")
+        return "YES"; 
+    } catch(error) {
+       return error.message
+    }
+ }
+ 
